28a7a7ff3ae7b833f27b015e2337ea309f686c9a
handongqingxue <1417140290@qq.com>
2022-11-09 14:11:15 +0800

    完善

--- src/main/java/com/znczCxtcGkj/util/APIUtil.java
+++ src/main/java/com/znczCxtcGkj/util/APIUtil.java
@@ -1,17 +1,26 @@
 package com.znczCxtcGkj.util;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import net.sf.jmimemagic.Magic;
 
+import net.sf.jmimemagic.MagicMatch;
+
+import org.apache.commons.lang.StringUtils;
 import org.json.JSONObject;
 
 import com.znczCxtcGkj.entity.*;
@@ -72,6 +81,118 @@ public class APIUtil {
 		JSONObject resultJO = new JSONObject(result);
 		return resultJO;
 	}
+	
+	/**
+	* 上传图片
+	* @param urlStr
+	* @param textMap
+	* @param fileMap
+	* @return
+	*/
+	public static JSONObject formUpload(String urlStr, Map textMap, Map fileMap) {
+		//https://blog.csdn.net/weixin_35674742/article/details/114192180
+		String result = null;
+		HttpURLConnection conn = null;
+		String BOUNDARY = "---------------------------123821742118716"; //boundary就是request头和上传文件内容的分隔符
+		try {
+			URL url = new URL(urlStr);
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setConnectTimeout(5000);
+			conn.setReadTimeout(30000);
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			conn.setUseCaches(false);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Connection", "Keep-Alive");
+			conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; zh-CN; rv:1.9.2.6)");
+			conn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);
+			
+			OutputStream out = new DataOutputStream(conn.getOutputStream());
+		
+			//text
+			if (textMap != null) {
+				StringBuffer strBuf = new StringBuffer();
+				Iterator iter = textMap.entrySet().iterator();
+				while (iter.hasNext()) {
+					Map.Entry entry = (Entry) iter.next();
+					String inputName = (String) entry.getKey();
+					String inputValue = (String) entry.getValue();
+					if (inputValue == null) {
+						continue;
+					}
+					strBuf.append("\r\n").append("--").append(BOUNDARY).append("\r\n");
+					strBuf.append("Content-Disposition: form-data; name=\"" + inputName + "\"\r\n\r\n");
+					strBuf.append(inputValue);
+				}
+				out.write(strBuf.toString().getBytes());
+			}
+	
+			//file
+			if (fileMap != null) {
+				Iterator iter = fileMap.entrySet().iterator();
+				while (iter.hasNext()) {
+					Map.Entry entry = (Entry) iter.next();
+					String inputName = (String) entry.getKey();
+					String inputValue = (String) entry.getValue();
+					if (inputValue == null) {
+						continue;
+					}
+					File file = new File(inputValue);
+					String filename = file.getName();
+					MagicMatch match = Magic.getMagicMatch(file, false, true);
+					String contentType = match.getMimeType();
+					StringBuffer strBuf = new StringBuffer();
+					strBuf.append("\r\n").append("--").append(BOUNDARY).append("\r\n");
+					strBuf.append("Content-Disposition: form-data; name=\"" + inputName + "\"; filename=\"" + filename + "\"\r\n");
+					strBuf.append("Content-Type:" + contentType + "\r\n\r\n");
+		
+					out.write(strBuf.toString().getBytes());
+		
+					DataInputStream in = new DataInputStream(new FileInputStream(file));
+		
+					int bytes = 0;
+					byte[] bufferOut = new byte[1024];
+					while ((bytes = in.read(bufferOut)) != -1) {
+						out.write(bufferOut, 0, bytes);
+					}
+					in.close();
+				}
+			}
+	
+			byte[] endData = ("\r\n--" + BOUNDARY + "--\r\n").getBytes();
+			out.write(endData);
+			out.flush();
+			out.close();
+	
+			// 读取返回数据
+			StringBuffer strBuf = new StringBuffer();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				strBuf.append(line).append("\n");
+			}
+			result = strBuf.toString();
+			System.out.println("result==="+result);
+		
+			reader.close();
+			reader = null;
+		} 
+		catch (Exception e) {
+			System.out.println("发送POST请求出错。" + urlStr);
+			e.printStackTrace();
+		} 
+		finally {
+			if (conn != null) {
+				conn.disconnect();
+				conn = null;
+			}
+		}
+		JSONObject resultJO = null;
+		if(!StringUtils.isBlank(result))
+			resultJO = new JSONObject(result);
+		return resultJO;
+	}
+
 
 	public static JSONObject getDingDanBySfzhZt(String sfzh, String ddztMc) {
 		JSONObject resultJO = null;
@@ -187,14 +308,19 @@ public class APIUtil {
 	}
 	
 	public static JSONObject testFile() {
-		File file = new File("D:/wear/202211/IMG_20221105_120734.jpg");
+		String file1Path = "D:/wear/202211/IMG_20221105_120734.jpg";
+		File file1 = new File(file1Path);
+		String file2Path = "D:/wear/202211/IMG_20221104_200132.jpg";
+		File file2 = new File(file2Path);
 		JSONObject resultJO = null;
 		try {
 			Map parames = new HashMap<String, Object>();
-			System.out.println("size==="+file.length());
-	        parames.put("file", file);
-	        resultJO = doHttp("testFile",parames);
-		} catch (IOException e) {
+			System.out.println("size1==="+file1.length());
+			System.out.println("size2==="+file2.length());
+	        parames.put("file1", file1Path);
+	        parames.put("file2", file2Path);
+	        formUpload(SERVICE_URL+"testFile",null,parames);
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
